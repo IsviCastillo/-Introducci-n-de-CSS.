9c82e091e1452b6166e1fc490812f22fb7aafc3e
IsviCastillo <isvijosecastillocruz@gmail.com>
2025-04-06 06:55:57 -0600

    Delete aprender.js

--- aprender.js
+++
@@ -1,9 +0,0 @@
-function mostrarHora(){
-    const ahora=new Date();
-    const hora=ahora.toLocaleTimeString()
-    document.getElementById("hora").textContent="Hora:"+hora;
-    
-}
-
-setInterval(mostrarHora,1000);
-
