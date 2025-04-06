5e9f1290f19a5df006086e88a5f9f0ea8055c497
IsviCastillo <isvijosecastillocruz@gmail.com>
2025-04-05 20:03:59 -0600

    Update aprender.js

--- aprender.js
+++ aprender.js
@@ -7,13 +7,3 @@ function mostrarHora(){
 
 setInterval(mostrarHora,1000);
 
-card.addEventListener('mouseenter', () => {
-    card.style.transform = 'scale(1.05)';
-    card.style.transition = 'transform 0.3s ease';
-    card.style.boxShadow = '0 8px 20px rgba(0, 0, 0, 0.3)';
-  });
-  
-  card.addEventListener('mouseleave', () => {
-    card.style.transform = 'scale(1)';
-    card.style.boxShadow = 'none';
-  });
